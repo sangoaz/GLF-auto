c3dbd5e40eccedb12f430d6be2e318233b6812a7
sangoaz <kevin.fruchon@gmail.com>
2026-05-11 12:19:02 +0200

    Déploiement Render

--- app/main.py
+++ app/main.py
@@ -33,7 +33,10 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://glf-auto-frontend.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
